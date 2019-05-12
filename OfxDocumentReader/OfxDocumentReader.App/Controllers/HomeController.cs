1a05c2b00e6fc2199fb842c37b9a40420623d857
brunno nunes <web.brunno@gmail.com>
2019-05-12 18:35:29 -0300

    Adds UploadFiles action method

--- OfxDocumentReader/OfxDocumentReader.App/Controllers/HomeController.cs
+++ OfxDocumentReader/OfxDocumentReader.App/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OfxDocumentReader.App.Models;
 
@@ -9,6 +13,33 @@ namespace OfxDocumentReader.App.Controllers
         public IActionResult Index()
         {
             return View(new List<TransactionViewModel>());
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            List<string> fileContent = new List<string>();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+
+                    using (var reader = new StreamReader(formFile.OpenReadStream()))
+                    {
+                        fileContent.AddRange(reader.ReadToEnd().Split("\r\n").ToList());
+                    }
+                }
+            }
+
+            return Ok();
         }
     }
 }
