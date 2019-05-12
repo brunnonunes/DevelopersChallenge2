a89322ecd7d4e859f35bb3abb86049b6a16b603f
brunno nunes <web.brunno@gmail.com>
2019-05-12 18:33:37 -0300

    Creates project

---
+++ OfxDocumentReader/OfxDocumentReader.App/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OfxDocumentReader.App.Models;
+
+namespace OfxDocumentReader.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<TransactionViewModel>());
+        }
+    }
+}
---
+++ OfxDocumentReader/OfxDocumentReader.App/Models/TransactionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OfxDocumentReader.App.Models
+{
+    public class TransactionViewModel
+    {
+        public string Type { get; set; }
+
+        public string DatePosted { get; set; }
+
+        public string Amount { get; set; }
+
+        public string Description { get; set; }
+    }
+}
